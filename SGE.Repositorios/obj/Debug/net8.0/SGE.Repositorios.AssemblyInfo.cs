b232216a8dca4b6067d88a790b685a33af8ad84c
Juan Pablo Acosta <Juampyacosta5@gmail.com>
2026-05-10 18:10:04 -0300

    Config: añadir vercel.json para manejo de rutas SPA

--- SGE.Repositorios/obj/Debug/net8.0/SGE.Repositorios.AssemblyInfo.cs
+++ SGE.Repositorios/obj/Debug/net8.0/SGE.Repositorios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39970b3a06a6694489ef68c19ca38c5e6af88e59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+530afb26f84a85ddd0fbf23b600d399e506c731e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
